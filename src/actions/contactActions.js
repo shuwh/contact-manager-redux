98c7ea379a75bd671028532d7cbf563124beabca
Winston <shuwh317@gmail.com>
2019-02-28 22:19:18 -0800

    GET Request dispatch, as well as POST $ DELETE

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,19 +1,25 @@
 import * as actionTypes from "./types";
+import axios from 'axios';
 
-export const getContacts = () => {
-  return {
-    type: actionTypes.GET_CONTACTS
-  };
-};
-export const deleteContact = id => {
-  return {
+export const getContacts = () => async (dispatch, getState) => {
+  const res = await axios.get('http://jsonplaceholder.typicode.com/users');
+  dispatch({
+    type: actionTypes.GET_CONTACTS,
+    payload: res.data,
+  })
+}
+
+export const deleteContact = id => async dispatch => {
+  await axios.delete(`http://jsonplaceholder.typicode.com/users/${id}`);
+  dispatch({
     type: actionTypes.DELETE_CONTACT,
     payload: id
-  };
+  });
 };
-export const addContact = contact => {
-  return {
+export const addContact = contact => async dispatch => {
+  const res = await axios.post('http://jsonplaceholder.typicode.com/users/', contact)
+  dispatch({
     type: actionTypes.ADD_CONTACT,
-    payload: contact
-  };
+    payload: res.data
+  });
 };
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,33 +1,15 @@
 import * as actionTypes from "../actions/types";
 
 const initState = {
-  contacts: [
-    {
-      id: 1,
-      name: "John Doe",
-      email: "john@gmail.com",
-      phone: "555-555-5555"
-    },
-    {
-      id: 2,
-      name: "Karen Williams",
-      email: "karen@gmail.com",
-      phone: "444-444-4444"
-    },
-    {
-      id: 3,
-      name: "Henry Johnson",
-      email: "henry@gmail.com",
-      phone: "333-333-333"
-    }
-  ]
+  contacts: []
 };
 
 export default (state = initState, action) => {
   switch (action.type) {
     case actionTypes.GET_CONTACTS:
       return {
-        ...state
+        ...state,
+        contacts: action.payload,
       };
     case actionTypes.DELETE_CONTACT:
       return {
